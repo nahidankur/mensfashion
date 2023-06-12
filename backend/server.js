e67602df7298c9e5a1e58bde96045dbd9aa42825
Nahid Karim Ankur <nahidankur@gmail.com>
2023-06-12 09:50:57 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,11 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-)
-
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
 
 
 app.use(notFound);
