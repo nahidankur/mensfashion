08ac6463da542412c1f15a9ca94ed67ecdce7ed5
Nahid Karim Ankur <nahidankur@gmail.com>
2023-06-12 09:52:23 +0600

    Update Meta.jsx

--- frontend/src/components/Meta.jsx
+++ frontend/src/components/Meta.jsx
@@ -11,9 +11,9 @@ const Meta = ({ title, description, keywords }) => {
 };
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
+  title: 'Welcome To MensFashion',
   description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  keywords: 'shoes, buy shirts, cheap bags',
 };
 
 export default Meta;
